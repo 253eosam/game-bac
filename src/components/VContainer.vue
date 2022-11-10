02e5abe17c23a62dfdbe8e896b56a2f889cb65ff
253eosam <253eosam@gmail.com>
2022-11-10 18:21:51 +0900

    모바일 화면 사이즈 조정

--- src/components/VContainer.vue
+++ src/components/VContainer.vue
@@ -1,22 +1,30 @@
 <template>
-  <div id="container">
+  <div id="container" class="transition-height duration-500 ease-in-out">
     <VModalVue />
     <div id="wrap" class="relative">
       <VHeader />
       <VContentVue />
       <VBottom v-if="hasBottom"/>
     </div>
-
   </div>
 </template>
 
 <script lang="ts">
-import { computed, defineComponent, watch } from 'vue'
+import { computed, defineComponent, onBeforeMount } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 import VBottom from './VBottom.vue'
 import VContentVue from './VContent.vue'
 import VHeader from './VHeader.vue'
 import VModalVue from './VModal.vue'
+
+const calcInitHeight = () => {
+  const applyHeight = () => {
+    let vh = window.innerHeight * 0.01
+    document.documentElement.style.setProperty("--vh", `${vh}px`)
+  }
+  applyHeight()
+  window.addEventListener('resize', applyHeight)
+}
 
 export default defineComponent({
   components: {
@@ -27,7 +35,10 @@ export default defineComponent({
   },
   setup() {
     const route = useRoute()
-
+    
+    onBeforeMount(() => {
+      calcInitHeight()
+    })
     return {
       hasBottom: computed(() => {
         return route.meta?.hasBottom ?? false
@@ -37,8 +48,12 @@ export default defineComponent({
 })
 </script>
 
-<style scoped>
-#container, #wrap {
+<style>
+#container {
+  height: 100vh;
+  height: calc(var(--vh, 1vh) * 100);
+}
+#wrap {
   width: 100%;
   height: 100%;
 }
