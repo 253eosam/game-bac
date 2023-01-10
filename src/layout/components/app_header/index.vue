c29d4d8e04e52d8b11cc8fc424a5f1039099bea5
Seongjun Lee <donaldd@wemakeprice.com>
2023-01-10 18:55:04 +0900

    Refac) app-header layout 수정

--- src/layout/components/app_header/index.vue
+++ src/layout/components/app_header/index.vue
@@ -1,14 +1,26 @@
 
 <template>
   <header>
-    <nav class="gnb">
-      <InfoIcon />
+    <nav class="gnb flex bg-primary">
+      <div>
+        <button class="p-1" @click="onClick" >
+          <InfoIcon />
+        </button>
+        <button class="p-1" @click="onClick" >
+          <RefreshIcon />
+        </button>
+        <button class="p-1" @click="onClick" >
+          <HomeIcon />
+        </button>
+      </div>
     </nav>
   </header>
 </template>
 
 <script setup lang="ts">
 import InfoIcon  from '@src/components/icons/InfoIcon.vue'
+import RefreshIcon  from '@src/components/icons/RefreshIcon.vue'
+import HomeIcon  from '@src/components/icons/HomeIcon.vue'
 
 </script>
 
